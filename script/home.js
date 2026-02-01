6d752540a82e98933366e29fb974bcbc84c8188b
Sikaroki <sonnam2k8@gmail.com>
2026-02-01 22:15:21 +0700

    update

--- script/home.js
+++ script/home.js
@@ -19,8 +19,6 @@ let state = '';
 
 /* navigator buttons active state */
 
-btnStart.classList.add('active');
-
 btn.forEach(button => {
   button.onclick = (e) => {
     btn.forEach(b => b.classList.remove('active'));
@@ -72,75 +70,86 @@ function switchSlide() {
   }
 }
 
-function addfocus(slide) {
+function addFocus(slide) {
   if (slide === 2) {
     nd2.classList.remove('active-nd');
     nd3.classList.remove('active-nd');
 
-    nd1.style.animation = 'highlight 0.3s';
     nd1.classList.add('active-nd');
-
-    nd1.addEventListener('animationend', () => {
-      indexS2 = 0;
-      switchContentS2();
-    }, {once: true})
+ 
+    indexS2 = 0;
+    switchContentS2();
   }
   else if (slide === 3) {
     usa.classList.remove('active-nd');
 
-    france.style.animation = 'highlight 0.3s';
-    france.classList.add('active-nd');
+    france.classList.add('active-des');
 
     picF.classList.add('focus');
     picF.classList.remove('blur');
 
     picU.classList.add('blur');
     picU.classList.remove('focus');
-
-    france.addEventListener('animationend', () => {
-      indexS3 = 0;
-      switchContentS3();
-    }, {once: true})
+    
+    indexS3 = 0;
+    switchContentS3();
   }
 }
 
-function resetImg(slide) {
+function resetFocus(slide) {
   if (slide === 1) {
     date2.style.opacity = '0';
     sliderImg.style.transform = 'translateX(0)';
+
+    nd2.classList.add('not-active-nd');
+    nd3.classList.add('not-active-nd');
   } 
   else if (slide === 2) {
      date2.style.opacity = '0';
   }
-  else {
+  else if (slide === 3) {
     sliderImg.style.transform = 'translateX(0)';
+
+    nd2.classList.add('not-active-nd');
+    nd3.classList.add('not-active-nd');
+  }
+  else if (slide === 4) {
+    date2.style.opacity = '0';
+    sliderImg.style.transform = 'translateX(0)';
+
+    nd2.classList.add('not-active-nd');
+    nd3.classList.add('not-active-nd');
   }
 }
 
 btnStart.addEventListener('click', () => {
   state = 's1';
+  indexS = 0;
   switchSlide();
-  resetImg(1);
+  resetFocus(1);
 })
 
 btn2.addEventListener('click', () => {
   state = 's2';
+  indexS = 1;
   switchSlide();
-  addfocus(2);
-  resetImg(2);
+  addFocus(2);
+  resetFocus(2);
 })
 
 btn3.addEventListener('click', () => {
   state = 's3';
+  indexS = 2;
   switchSlide();
-  addfocus(3);
-  resetImg(3);
+  addFocus(3);
+  resetFocus(3);
 })
 
 btn4.addEventListener('click', () => {
   state = 's4';
+  indexS = 3;
   switchSlide();
-  resetImg(1);
+  resetFocus(4);
 })
 
 /* highlight for silde 2 */
@@ -156,10 +165,10 @@ function moveImg(index) {
     sliderImg.style.transform = 'translateX(0)';
   }
   else if (index === 1) {
-    sliderImg.style.transform = 'translateX(-400px)';
+    sliderImg.style.transform = 'translateX(-600px)';
   }
   else {
-    sliderImg.style.transform = 'translateX(-800px)';
+    sliderImg.style.transform = 'translateX(-1200px)';
   }
 }
 
@@ -167,27 +176,39 @@ function switchContentS2() {
   switch(indexS2) {
     case 0: 
     nd1.classList.add('active-nd');
-
+    nd1.classList.remove('not-active-nd');
+    
     nd2.classList.remove('active-nd');
+    nd2.classList.add('not-active-nd');
+
     nd3.classList.remove('active-nd');
+    nd3.classList.add('not-active-nd');
 
     moveImg(0);
     break;
 
   case 1: 
     nd2.classList.add('active-nd');
+    nd2.classList.remove('not-active-nd');
 
     nd1.classList.remove('active-nd');
+    nd1.classList.add('not-active-nd');
+
     nd3.classList.remove('active-nd');
+    nd3.classList.add('not-active-nd');
 
     moveImg(1);
     break;
 
   case 2: 
     nd3.classList.add('active-nd');
+    nd3.classList.remove('not-active-nd');
+
+    nd2.classList.remove('active-nd');
+    nd2.classList.add('not-active-nd');
 
     nd1.classList.remove('active-nd');
-    nd2.classList.remove('active-nd');
+    nd1.classList.add('not-active-nd');
 
     moveImg(2);
     break;
@@ -197,9 +218,11 @@ function switchContentS2() {
 function switchContentS3() {
   switch(indexS3) {
     case 0:
-      france.classList.add('active-nd');
+      france.classList.add('active-des');
+      france.classList.remove('not-active-nd');
 
-      usa.classList.remove('active-nd');
+      usa.classList.remove('active-des2');
+      usa.classList.add('not-active-nd');
 
       date1.style.opacity = '1';
       date2.style.opacity = '0';
@@ -211,9 +234,11 @@ function switchContentS3() {
       picU.classList.remove('focus');
       break;
     case 1:
-      usa.classList.add('active-nd');
+      usa.classList.add('active-des2');
+      usa.classList.remove('not-active-nd');
 
-      france.classList.remove('active-nd');
+      france.classList.remove('active-des');
+      france.classList.add('not-active-nd');
 
       date1.style.opacity = '0';
       date2.style.opacity = '1';
@@ -262,4 +287,75 @@ window.addEventListener('keydown', function(e) {
     indexS3--;
     switchContentS3();
   }
+
+  if (e.key === 'ArrowDown' && indexS < 3) {
+    indexS++;
+    changeSlide();
+  }
+  else if (e.key === 'ArrowUp' && indexS > 0) {
+    indexS--;
+    changeSlide();
+  }
 })
+
+let indexS = 0;
+
+function changeSlide() {
+  switch(indexS) {
+    case 1:
+      state = 's2';
+      switchSlide();
+      addFocus(2);
+      resetFocus(2);
+
+      btn2.classList.add('active');
+      
+      btnStart.classList.remove('active');
+      btn3.classList.remove('active');
+      btn4.classList.remove('active');
+      break;
+
+    case 2:
+      state = 's3';
+      switchSlide();
+      addFocus(3);
+      resetFocus(3);
+
+      btn3.classList.add('active');
+      
+      btnStart.classList.remove('active');
+      btn2.classList.remove('active');
+      btn4.classList.remove('active');
+      break;
+
+    case 3:
+      state = 's4';
+      switchSlide();
+      resetFocus(4);
+
+      btn4.classList.add('active');
+      
+      btnStart.classList.remove('active');
+      btn3.classList.remove('active');
+      btn2.classList.remove('active');
+      break;
+
+    case 0:
+      state = 's1';
+      switchSlide();
+      resetFocus(1);
+
+      btnStart.classList.add('active');
+      
+      btn2.classList.remove('active');
+      btn3.classList.remove('active');
+      btn4.classList.remove('active');
+      break;
+  }
+}
+
+/* default start */
+
+btnStart.classList.add('active');
+nd1.classList.add('active-nd');
+france.classList.add('active-des');
